5d6ce99953efe73d4e562c21671823bd1db69d0e
piyush777 <piyushgandhi092@gmail.com>
2024-09-11 11:21:08 +0530

    Sorting

---
+++ src/main/java/org/ust/sorting/InsertionSortDemo.java
@@ -0,0 +1,39 @@
+package org.ust.sorting;
+
+public class InsertionSortDemo {
+    public static void insertionSort(int[] array) {
+        int n = array.length;
+        for (int i = 1; i < n; i++) {
+            int key = array[i];
+            int j = i - 1;
+
+            // Move elements of array[0..i-1], that are greater than key,
+            // to one position ahead of their current position
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = key;
+            System.out.println("After pass: ");
+            for(int num: array){
+                System.out.print (num+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12, 11, 13, 5, 6};
+        System.out.println("Original Array:");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+
+        insertionSort(arr);
+
+        System.out.println("\nSorted Array:");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
